eeed7631bbf0ec81028ccb79800d94b253925767
Anton <69902586+ant0n7@users.noreply.github.com>
2022-03-03 13:22:59 +0100

    Enhance security.
    Add PreAuthorize annotations so only users with the role ADMIN can access endpoints for now.

--- src/main/java/com/example/demo/domain/appUser/UserController.java
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -29,12 +29,14 @@ public class UserController {
     @Autowired
     private SecurityService securityService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "List of all users.", description = "Get a list of all users with all their information.")
     @GetMapping("/")
     public ResponseEntity<Collection<User>> findAll() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Save a single user.", description = "Save a single user to the database. The API automatically encrypts the password with BCrypt and generates an UUID.")
     @PostMapping("/")
     public ResponseEntity<User> save(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Single User object") @Valid @RequestBody User user)
@@ -42,12 +44,14 @@ public class UserController {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Save a single role.", description = "Save a single role to the database. The API automatically generates an UUID.")
     @PostMapping("/role")
     public ResponseEntity<Role> save(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Single Role object") @Valid @RequestBody Role role) {
         return new ResponseEntity<>(userService.saveRole(role), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Get an user by username.", description = "Receive a single user with all available Information by its username.")
     @GetMapping("/{username}")
     public ResponseEntity<User> getByUsername(@Parameter(name = "Username", description = "Unique username of the user requested") @PathVariable String username) {
@@ -64,12 +68,14 @@ public class UserController {
         return new ResponseEntity<>(userService.getUsersOfGroup(groupname, pageable), HttpStatus.FOUND);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Get an user by ID.", description = "Receive a single user with all available Information by its UUID.")
     @GetMapping("/byId/{id}")
     public ResponseEntity<User> getById(@Parameter(description = "UUID of the user requested") @PathVariable UUID id) throws InstanceNotFoundException {
         return new ResponseEntity<>(userService.findById(id).orElse(null), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Add a role to a user.", description = "Add a single role to a single user. There won't be any " +
             "loss of roles as it just adds a role and replaces any roles.")
     @PostMapping("/{username}/role/{rolename}")
@@ -83,7 +89,7 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Update a user by ID.", description = "Update a single user by its ID. Pass the whole new user in the " +
             "body and its ID in the path. If there's no user by that ID, nothing will change.")
     @PutMapping("/{id}")
@@ -91,7 +97,7 @@ public class UserController {
         return new ResponseEntity<>(userService.updateUser(id, user), HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Delete a user by ID.", description = "Delete a single user by its ID.")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@Parameter(description = "UUID of the user to delete.") @PathVariable UUID id) throws InstanceNotFoundException {
